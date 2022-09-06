32a6635be664c727765c69e08c4c2649ae4df8c6
Joonas Praks <joonaspraks@gmail.com>
2022-09-07 02:44:58 +0300

    forecast data in cards

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,99 @@
+<template>
+  <q-card class="my-card" flat bordered>
+    <q-item>
+      <q-item-section>
+        <q-item-label class="text-h4">{{ name }}</q-item-label>
+      </q-item-section>
+    </q-item>
+
+    <q-separator />
+
+    <q-card-section horizontal>
+      <q-card-section class="col-6">
+        <q-card-section class="text-h6">Day</q-card-section>
+
+        <q-icon :name="iconMap[dayPhenomenon]" color="primary" size="56px" />
+        <q-card-section>{{ dayPhenomenon }}</q-card-section>
+        <q-card-section>Max temp: {{ dayTempmax }}</q-card-section>
+      </q-card-section>
+
+      <q-separator vertical />
+      <q-card-section class="col-6">
+        <q-card-section class="text-h6">Night</q-card-section>
+
+        <q-icon :name="iconMap[nightPhenomenon]" color="primary" size="56px" />
+        <q-card-section>{{ nightPhenomenon }}</q-card-section>
+        <q-card-section>Min temp: {{ nightTempmin }}</q-card-section>
+      </q-card-section>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'EssentialLink',
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    nightTempmin: {
+      type: Number,
+      required: true,
+    },
+    dayTempmax: {
+      type: Number,
+      required: true,
+    },
+    nightPhenomenon: {
+      type: String,
+      required: true,
+    },
+    dayPhenomenon: {
+      type: String,
+      required: true,
+    },
+  },
+  setup() {
+    return {
+      iconMap: {
+        Clear: 'sunny',
+        'Few clouds': 'sunny',
+        'Variable clouds': 'cloud',
+        'Cloudy with clear spells': 'cloud',
+        Cloudy: 'cloud',
+        'Light snow shower': 'ac_unit',
+        'Moderate snow shower': 'ac_unit',
+        'Heavy snow shower': 'ac_unit',
+        'Light shower': 'water_drop',
+        'Moderate shower': 'water_drop',
+        'Heavy shower': 'water_drop',
+        'Light rain': 'water_drop',
+        'Moderate rain': 'water_drop',
+        'Heavy rain': 'water_drop',
+        'Risk of glaze': 'ac_unit',
+        'Light sleet': 'ac_unit',
+        'Moderate sleet': 'ac_unit',
+        'Light snowfall': 'ac_unit',
+        'Moderate snowfall': 'ac_unit',
+        'Heavy snowfall': 'severe_cold',
+        Snowstorm: 'severe_cold',
+        'Drifting snow': 'severe_cold',
+        Hail: 'ac_unit',
+        Mist: 'cloud',
+        Fog: 'cloud',
+        Thunder: 'flash_on',
+        Thunderstorm: 'thunderstorm',
+      },
+    };
+  },
+});
+</script>
+
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 600px
+</style>
--- src/components/CarouselComponent.vue
+++ src/components/CarouselComponent.vue
@@ -9,19 +9,17 @@
       class="rounded-borders"
     >
       <template
-        v-for="place in store.forecastToday.day.place"
+        v-for="(place, index) in store.forecastToday.day.place"
         :key="place.name"
       >
         <q-carousel-slide :name="place.name" class="column no-wrap flex-center">
-          <q-icon
-            :name="iconMap[place.phenomenon]"
-            color="primary"
-            size="56px"
+          <card-component
+            :name="place.name"
+            :dayTempmax="place.tempmax"
+            :nightTempmin="store.forecastToday.night.place[index].tempmin"
+            :dayPhenomenon="place.phenomenon"
+            :nightPhenomenon="store.forecastToday.night.place[index].phenomenon"
           />
-          <div class="q-mt-md text-center">
-            <li>Phenomenon: {{ place.phenomenon }}</li>
-            <li>Max temp: {{ place.tempmax }}</li>
-          </div>
         </q-carousel-slide>
       </template>
     </q-carousel>
@@ -45,47 +43,18 @@
 import { useQuasar } from 'quasar';
 import { ref, watchEffect } from 'vue';
 import { useForecastStore } from '../stores/forecast-store';
+import CardComponent from './CardComponent.vue';
 
 export default {
   setup() {
     const q = useQuasar();
     q.loading.show();
-
     watchEffect(() => useForecastStore().forecastList && q.loading.hide());
-
     return {
       slide: ref(''),
       store: useForecastStore(),
-      iconMap: {
-        Clear: 'sunny',
-        'Few clouds': 'sunny',
-        'Variable clouds': 'cloud',
-        'Cloudy with clear spells': 'cloud',
-        Cloudy: 'cloud',
-        'Light snow shower': 'ac_unit',
-        'Moderate snow shower': 'ac_unit',
-        'Heavy snow shower': 'ac_unit',
-        'Light shower': 'water_drop',
-        'Moderate shower': 'water_drop',
-        'Heavy shower': 'water_drop',
-        'Light rain': 'water_drop',
-        'Moderate rain': 'water_drop',
-        'Heavy rain': 'water_drop',
-        'Risk of glaze': 'ac_unit',
-        'Light sleet': 'ac_unit',
-        'Moderate sleet': 'ac_unit',
-        'Light snowfall': 'ac_unit',
-        'Moderate snowfall': 'ac_unit',
-        'Heavy snowfall': 'severe_cold',
-        Snowstorm: 'severe_cold',
-        'Drifting snow': 'severe_cold',
-        Hail: 'ac_unit',
-        Mist: 'cloud',
-        Fog: 'cloud',
-        Thunder: 'flash_on',
-        Thunderstorm: 'thunderstorm',
-      },
     };
   },
+  components: { CardComponent },
 };
 </script>
